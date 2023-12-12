b4f09c848d6a0c226438787c344dfa20d417d799
GabLRamon <gabramon3@gmail.com>
2023-12-12 10:46:36 -0300

    Initial commit

---
+++ src/main/java/com/example/gabCustomer/GabCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gabCustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gabCustomer/GabCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gabCustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
