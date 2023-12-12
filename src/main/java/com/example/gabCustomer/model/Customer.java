543ad91156777ebf893ea9d4fe83746ace5817a8
GabLRamon <gabramon3@gmail.com>
2023-12-12 11:22:12 -0300

    update

--- src/main/java/com/example/gabCustomer/model/Customer.java
+++ src/main/java/com/example/gabCustomer/model/Customer.java
@@ -1,5 +1,6 @@
 package com.example.gabCustomer.model;
 
+import com.example.gabCustomer.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -18,9 +19,19 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 
+    @CourseCode
+    private String courseCode;
+
     public Customer() {
     }
 
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
---
+++ src/main/java/com/example/gabCustomer/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,21 @@
+package com.example.gabCustomer.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String code, ConstraintValidatorContext constraintValidatorContext) {
+        boolean result = code.startsWith(coursePrefix);
+
+        return result;
+    }
+}
